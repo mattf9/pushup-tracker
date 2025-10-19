e18945a116849514e78fcbee9906a1a2fd4f9221
nex9 <nex9@corecompute.com>
2025-10-19 19:55:46 -0400

    mockup of main page with static test data

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,25 @@
+from flask import render_template
 from app import app
+from datetime import datetime
+from app.forms import PushupForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "<center><h1>Welcome to Pushup Tracker!</h1></center>"
+    user = {'username': 'Joe'}
+
+    pushup_sets = [
+        {
+            'username': 'Joe',
+            'pushup_count': 15,
+            'pushup_time': "2025-10-19 08:16:57"
+        },
+        {
+            'username': 'Joe',
+            'pushup_count': 20,
+            'pushup_time': "2025-10-19 08:16:57"
+        }
+    ]
+    form = PushupForm()
+    return render_template('index.html', title='Home', user=user, pushup_sets=pushup_sets, form=form)
+    # return render_template('index.html')
