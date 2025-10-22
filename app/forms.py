407ccbd09cbedb3a00663b70def693270f0276db
mattf9 <matt@mattzone.com>
2025-10-22 10:43:01 -0400

    login form and database scaffolding

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, IntegerRangeField, StringField, SubmitField
+from wtforms import BooleanField, IntegerField, PasswordField, StringField, SubmitField
 from wtforms.validators import DataRequired, NumberRange
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
 
 class PushupForm(FlaskForm):
     pushup_count = IntegerField('Number of pushups', default=10, validators=[DataRequired(), NumberRange(min=1, max=100)])
